9a4e44591af8ccf00ebeb8bff4a0bbefd2a272d4
BoRaT-7 <boratabid@gmail.com>
2025-07-14 00:30:30 +0600

    commit-3

--- devlop/js/main.js
+++ devlop/js/main.js
@@ -1,11 +1,27 @@
 const blogButton = document.getElementById('blog_button');
 const mainBalance = document.getElementById('main_balance');
-
 blogButton.addEventListener('click',function(){
     blogButton.classList.add('bg-red-400');
     mainBalance.classList.remove('bg-red-400');
-})
+});
 mainBalance.addEventListener('click',function(){
     mainBalance.classList.add('bg-red-400');
     blogButton.classList.remove('bg-red-400');
-})
+});
+
+const donationButton = document.getElementById('donation_button');
+const historyButton = document.getElementById('historay_button');
+const allCard = document.getElementById('all_card');
+donationButton.addEventListener('click',function(){
+    allCard.classList.remove('hidden');
+    donationButton.classList.add('bg-green-400');
+    historyButton.classList.remove('bg-green-400');
+});
+historyButton.addEventListener('click', function(){
+  historyButton.classList.add('bg-green-400');
+  historyButton.classList.remove('bg-green-400');
+  window.location.href = 'history_pages.html';
+
+});
+
+
