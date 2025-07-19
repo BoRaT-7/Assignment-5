80693a15d3272a91ad0b216b1c4a4fe178b096eb
Md Bocktear Abid Borat <abidborat@gmail.com>
2025-07-19 23:27:04 +0600

    commit-2

--- devlop/js/main.js
+++ devlop/js/main.js
@@ -8,3 +8,19 @@ document.getElementById('card_balance').addEventListener ('click',function(){
     cardBalance.classList.add('bg-red-400');
      blogBtn.classList.remove('bg-red-400');
 });
+
+
+const donationBtn = document.getElementById('donation_button');
+const historyBtn = document.getElementById('historay_button');
+const allCard = document.getElementById('all_card');
+donationBtn.addEventListener('click',function(){
+    allCard.style.display = 'block';
+    allCard.classList.add('bg-blue-500');
+
+    donationBtn.classList.add('bg-blue-500');
+    historyBtn.classList.remove('bg-blue-500');
+});
+
+historyBtn.addEventListener('click',function(){
+    window.location.href ='history_pages.html';
+})
