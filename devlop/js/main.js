4827b32a66e8e3ec7fae91b61fcf8b16e98f2f4a
Md Bocktear Abid Borat <abidborat@gmail.com>
2025-07-19 22:09:55 +0600

    commit-2

--- devlop/js/main.js
+++ devlop/js/main.js
@@ -1,27 +1,10 @@
-const blogButton = document.getElementById('blog_button');
-const mainBalance = document.getElementById('main_balance');
-blogButton.addEventListener('click',function(){
-    blogButton.classList.add('bg-red-400');
-    mainBalance.classList.remove('bg-red-400');
+const blogBtn = document.getElementById('blog_button');
+const cardBalance = document.getElementById('card_balance');
+document.getElementById('blog_button').addEventListener ('click',function(){
+    blogBtn.classList.add('bg-red-400');
+    cardBalance.classList.remove('bg-red-400');
 });
-mainBalance.addEventListener('click',function(){
-    mainBalance.classList.add('bg-red-400');
-    blogButton.classList.remove('bg-red-400');
+document.getElementById('card_balance').addEventListener ('click',function(){
+    cardBalance.classList.add('bg-red-400');
+     blogBtn.classList.remove('bg-red-400');
 });
-
-const donationButton = document.getElementById('donation_button');
-const historyButton = document.getElementById('historay_button');
-const allCard = document.getElementById('all_card');
-donationButton.addEventListener('click',function(){
-    allCard.classList.remove('hidden');
-    donationButton.classList.add('bg-green-400');
-    historyButton.classList.remove('bg-green-400');
-});
-historyButton.addEventListener('click', function(){
-  historyButton.classList.add('bg-green-400');
-  historyButton.classList.remove('bg-green-400');
-  window.location.href = 'history_pages.html';
-
-});
-
-
